60d7f203f2d2f5a6930565a294168680f80a908a
dazarcenko <dazarcenko@gmail.com>
2026-09-15 01:12:51 +0300

    Update start.js

--- casic/start.js
+++ casic/start.js
@@ -1,9 +1,14 @@
 const { Telegraf, Markup } = require('telegraf');
+const express = require('express');
+const cors = require('cors'); // Подключаем библиотеку
 
+const app = express();
+app.use(cors()); // Разрешаем запросы с любых доменов
+app.use(express.json());
 // Замени на токен от BotFather
 const bot = new Telegraf(process.env.BOT_TOKEN);
 // Замени на HTTPS-ссылку, куда ты залил index.html
-const WEB_APP_URL = 'neskcasino-production.up.railway.app'; 
+const WEB_APP_URL = 'https://neskcasino-production.up.railway.app/'; 
 
 bot.command('start', (ctx) => {
     ctx.reply(
