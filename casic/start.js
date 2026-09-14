6128dc1f1ed23bb935f263c79092636729ac0577
dazarcenko <dazarcenko@gmail.com>
2026-09-15 00:32:24 +0300

    Update start.js

--- casic/start.js
+++ casic/start.js
@@ -1,7 +1,7 @@
 const { Telegraf, Markup } = require('telegraf');
 
 // Замени на токен от BotFather
-const bot = new Telegraf('ТВОЙ_ТОКЕН_БОТА'); 
+const bot = new Telegraf(process.env.BOT_TOKEN);
 // Замени на HTTPS-ссылку, куда ты залил index.html
 const WEB_APP_URL = 'https://твой-сайт.com'; 
 
